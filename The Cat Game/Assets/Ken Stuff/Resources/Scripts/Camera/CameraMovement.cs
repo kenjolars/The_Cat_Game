f3ffd8c031edfacd8042a4bd8c36f0ae5acf6050
kenjolars <ken.phamswagyolo@outlook.com>
2022-04-06 03:31:19 +1000

    Controller and Faux Gravity
    Was able to somewhat make a player controller that was simple to movement and a simple third person camera needs some work and lastly was able to get the gravity working between planets

---
+++ The Cat Game/Assets/Ken Stuff/Resources/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // inspector variables
+    [SerializeField, Tooltip("Player transform for camera to follow")]
+    private Transform playerTransform;
+    [SerializeField, Tooltip("Camera offset from player (x not used)")]
+    private Vector3 offsetPosition = new Vector3(0, 5, 5);
+    [SerializeField]
+    private bool lookAt = true;
+
+    // privates
+    private Transform _mainCam = null;
+
+    // Use this for initialization
+    private void Start()
+    {
+        if(playerTransform == null)
+        {
+            Debug.LogError("CameraMovement is missing playerTransform");
+        }
+        else
+        {
+            _mainCam = Camera.main.transform;
+        }
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        UpdateCamera();
+    }
+
+    /// <summary>
+    /// Update camera position and rotation
+    /// </summary>
+    private void UpdateCamera()
+    {
+        if (playerTransform == null)
+        {
+            return;
+        }
+        // camera rig position
+        transform.position = playerTransform.position + -(playerTransform.forward * offsetPosition.z) + (playerTransform.up * offsetPosition.y);
+        // point camera at player
+        if (lookAt)
+        {
+            // point camera at player using players up direction
+            _mainCam.LookAt(playerTransform, playerTransform.up);
+        }
+        else
+        {
+            _mainCam.LookAt(playerTransform);
+        }
+    }
+}
---
+++ The Cat Game/Assets/Ken Stuff/Resources/Scripts/World/FakeGravity.cs
@@ -0,0 +1,62 @@
+﻿/*********************************************************************************************************
+ * The FakeGravity class should be place on your world object that other objects are to be drawn to.
+ * *******************************************************************************************************/ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeGravity : MonoBehaviour {
+
+    // inspector variables
+    [SerializeField, Tooltip("Amount of gravity to be applied to objects")]
+    private float gravity = -10;
+    [SerializeField, Tooltip("Planet Radius")]
+    private float size = 10;
+
+    // privates
+    private string _worldObjectTag = "World";
+    private float _objRotSpeed = 50;
+    private float _gravityBoost = 0;
+
+    // properties
+    public float WorldSize { get { return size; } }
+
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        gameObject.tag = _worldObjectTag;
+    }
+
+    /// <summary>
+    /// Generates false gravity for world objects 
+    /// </summary>
+    /// <param name="objBody"></param>
+    public void Attract(Transform objBody)
+    {
+        // set planet gravity direction for the object body
+        Vector3 gravityDir = (objBody.position - transform.position).normalized;
+        Vector3 bodyUp = objBody.up;
+        // apply gravity to objects rigidbody
+        objBody.GetComponent<Rigidbody>().AddForce(gravityDir * (gravity + _gravityBoost));
+        // update the objects rotation in relation to the planet
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityDir) * objBody.rotation;
+        // smooth rotation
+        objBody.rotation = Quaternion.Slerp(objBody.rotation, targetRotation, _objRotSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Function to increase world gravity during transfer
+    /// </summary>
+    /// <param name="increaseGravity"></param>
+    public void IncreaseGravity(bool increaseGravity, float amount)
+    {
+        if(increaseGravity)
+        {
+            _gravityBoost = gravity * amount;
+        }
+        else
+        {
+            _gravityBoost = 0;
+        }
+    }
+}
