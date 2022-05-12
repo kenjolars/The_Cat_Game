9a02f5c032d21f11257bbffd571622bbb6a50470
kenjolars <ken.phamswagyolo@outlook.com>
2022-05-13 05:56:13 +1000

    Getting rid of some files and fully implemented  UI with taming mechanic

--- Assets/Ken Stuff/Script/RadialMenu.cs
+++ Assets/Ken Stuff/Script/RadialMenu.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+public class RadialMenu : MonoBehaviour
 {
     //Variables
     public static bool GameIsPaused = false;
 
-    public GameObject pauseMenuUI;
+    public GameObject radialMenuUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenuUI.SetActive(false);
+        radialMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
@@ -20,32 +20,32 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
 
         {
             if (GameIsPaused)
             {
                 Resume();
-                GetComponent<RadialMenu>().enabled = true;
+                GetComponent<PauseMenu>().enabled = true;
             }
             else
             {
                 Pause();
-                GetComponent<RadialMenu>().enabled = false;
+                GetComponent<PauseMenu>().enabled = false;
             }
         }
     }
 
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
+        radialMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     void Pause()
     {
-        pauseMenuUI.SetActive(true);
+        radialMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
