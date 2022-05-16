75b10ada99257cfedcb40d8b7ea0d6a94de097cc
kenjolars <ken.phamswagyolo@outlook.com>
2022-05-16 20:52:37 +1000

    Final submission

--- Assets/Ken Stuff/Script/MainMenu.cs
+++ Assets/Ken Stuff/Script/MainMenu.cs
@@ -38,7 +38,7 @@ public class MainMenu : MonoBehaviour
 
     public void Tutorial()
     {
-        SceneManager.LoadScene(6);
+        SceneManager.LoadScene(5);
     }
 
     public void Level1()
@@ -51,7 +51,7 @@ public class MainMenu : MonoBehaviour
     }
     public void Level3()
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(6);
     }
     public void Level4()
     {
