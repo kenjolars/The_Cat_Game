721f9f9e12030ca4e4a1ca2d606f1b68c30255fc
kenjolars <ken.phamswagyolo@outlook.com>
2022-05-16 17:49:08 +1000

    Finalizing all of the levels and polish all transitions between scenes

--- Assets/Ken Stuff/Script/MainMenu.cs
+++ Assets/Ken Stuff/Script/MainMenu.cs
@@ -38,7 +38,7 @@ public class MainMenu : MonoBehaviour
 
     public void Tutorial()
     {
-        //SceneManager.LoadScene(0);
+        SceneManager.LoadScene(6);
     }
 
     public void Level1()
@@ -51,10 +51,10 @@ public class MainMenu : MonoBehaviour
     }
     public void Level3()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(5);
     }
     public void Level4()
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(3);
     }
 }
